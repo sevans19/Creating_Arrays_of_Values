46e670725568c7949a9e53b07d01f5d8eedd426a
sevans19 <31357592+sevans19@users.noreply.github.com>
2017-09-12 19:30:17 -0600

    Initial Commit
    learned about arrays and how to initilize them and use them

---
+++ Creating_Arrays_of_Values/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Creating_Arrays_of_Values
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // a bucket that contains other buckets 
+            // a grouping of data
+
+            // declare by starting with a data type and then
+            // declare how large the array will be 
+
+            // can access any item in the array by using its index 
+
+            // arrays use [] 
+            // [] --> array or index access operator 
+
+            // "new int[5]" defines the array as having 5 elements
+            //  int[] numbers = new int[5];
+
+            // defining an array of integers 
+            // "The numbers array at index one equals the value 8" 
+            // arrays are 0 based!!!!!!!
+            //  numbers[0] = 4;
+            //  numbers[1] = 8;
+            //  numbers[2] = 15;
+            //  numbers[3] = 16;
+            //  numbers[4] = 23;
+            // numbers[5] = 42;   ---> outside of the array
+
+            /*
+            int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
+            
+            Console.WriteLine(numbers[1].ToString());
+            Console.ReadLine();
+            */
+
+
+
+            string[] names = new string[] { "Eddie", "Alex", "Michael", "David Lee" };
+
+            // iterates through the code block once for each element in the array 
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
