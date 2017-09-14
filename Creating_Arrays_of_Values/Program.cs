e9a11a36f52dd3a285998f9306ab31ae97acef83
sevans19 <31357592+sevans19@users.noreply.github.com>
2017-09-13 18:36:45 -0600

    Second Commit
    learned about converting a string to an array of characters and how to reverse the elements in an array and how to find out the number of elements in an array by using the length property

--- Creating_Arrays_of_Values/Program.cs
+++ Creating_Arrays_of_Values/Program.cs
@@ -18,17 +18,20 @@ namespace Creating_Arrays_of_Values
             // [] --> array or index access operator 
 
             // "new int[5]" defines the array as having 5 elements
-            //  int[] numbers = new int[5];
+            int[] numbers = new int[5];
 
             // defining an array of integers 
             // "The numbers array at index one equals the value 8" 
             // arrays are 0 based!!!!!!!
-            //  numbers[0] = 4;
-            //  numbers[1] = 8;
-            //  numbers[2] = 15;
-            //  numbers[3] = 16;
-            //  numbers[4] = 23;
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
             // numbers[5] = 42;   ---> outside of the array
+
+            // the length property shows the number of elements in an array 
+            Console.WriteLine(numbers.Length);
 
             /*
             int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
@@ -38,7 +41,7 @@ namespace Creating_Arrays_of_Values
             */
 
 
-
+            /*
             string[] names = new string[] { "Eddie", "Alex", "Michael", "David Lee" };
 
             // iterates through the code block once for each element in the array 
@@ -47,6 +50,25 @@ namespace Creating_Arrays_of_Values
                 Console.WriteLine(name);
             }
             Console.ReadLine();
+            */
+
+            /*
+            string zig = "You can get what you want out of life " +
+                "if you help enough other people get what they want.";
+
+            // char can store a single alpha numberic character
+            // defining an array of chars 
+                // each individual letter of the string will be an element of the char array
+            char[] charArray = zig.ToCharArray();
+            // it takes each element in the array and reverse its ordinal position 
+            Array.Reverse(charArray);
+
+            // iterate through each char in the char array 
+            foreach (char zigChar in charArray)
+                Console.Write(zigChar);
+
+            Console.ReadLine();
+            */
 
 
         }
